f3fe91c39701741e6376f5f6e79c15c69e06349b
ethankook <93116378+ethankook@users.noreply.github.com>
2026-05-07 14:40:23 -0700

    feat: add login component, configure routing, and update environment setup

---
+++ frontend/src/app/core/auth/login/login.ts
@@ -0,0 +1,38 @@
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  loading = signal(false);
+  error = signal<string | null>(null);
+
+  async onSubmit(username: string, password: string) {
+    this.error.set(null);
+    this.loading.set(true);
+
+    try {
+      await this.auth.login({ username, password });
+      await this.router.navigate(['/']);
+    } catch (err) {
+      if (err instanceof HttpErrorResponse) {
+        this.error.set(err.error.message ?? 'Login failed.');
+      } else {
+        this.error.set('An unexpected error occurred.');
+      }
+    } finally {
+      this.loading.set(false);
+    }
+  }
+}
+
+
